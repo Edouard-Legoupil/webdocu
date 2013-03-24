f42397b17cbe63237e9543fc3713ca8d00421245
Edouard <edouard.legoupil@gmail.com>
2013-03-24 10:21:39 -0700

    Updated javascripts/popcorn.parserJSON.js

---
+++ javascripts/popcorn.parserJSON.js
@@ -0,0 +1,25 @@
+// PARSER: 0.3 JSON
+
+(function (Popcorn) {
+  Popcorn.parser( "parseJSON", "JSON", function( data ) {
+
+    // declare needed variables
+    var retObj = {
+          title: "",
+          remote: "",
+          data: []
+        },
+        manifestData = {}, 
+        dataObj = data.data;
+    
+    document.getElementById("title-container").innerHTML = data.title;
+    document.getElementById("desc-container").innerHTML = data.description;
+    
+    Popcorn.forEach( dataObj.data, function ( obj, key ) {
+      retObj.data.push( obj );
+    });
+
+    return retObj;
+  });
+
+})( Popcorn );
